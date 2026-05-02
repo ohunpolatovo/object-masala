3ea52587daa542bcc7ebc1de940444b9fba97602
olloberdi <ohunpolatov.o@gmail.com>
2026-05-02 14:25:41 +0500

    js-masalalar

---
+++ main.js
@@ -0,0 +1,41 @@
+// 1. user nomli object yarating. Ichida: name va age bo‘lsin. Keyin faqat name ni console.log qiling.
+let user = {
+  name: "Olloberdi",
+  age: 19,
+};
+console.log(user.name);
+
+// 2. product object yarating. Ichida: title va price bo‘lsin. Keyin price ni chiqaring.
+let product = {
+  title: "Najot talim",
+  price: 1200,
+};
+console.log(product.price);
+
+// 3. student object yarating. Ichida: name va active (true yoki false) bo‘lsin. Keyin active ni chiqaring.
+let student = {
+  name: "Olloberdi",
+  active: true,
+};
+console.log(student.active);
+
+// 4. car object yarating. Ichida: brand, color, year bo‘lsin. Keyin color ni chiqaring.
+let car = {
+  brand: "Malibu",
+  color: "black",
+  year: 2025,
+};
+console.log();
+
+// 5. phone object yarating. Ichida: brand va model bo‘lsin. Keyin model ni ['model'] orqali chiqaring.
+let phone = {};
+// 6. user object yarating. Ichida: name va address object bo‘lsin. address ichida: city bo‘lsin. Keyin
+// city ni chiqaring.
+// 7. users nomli array yarating. Ichida 2 ta object bo‘lsin (name va age bilan). Keyin birinchi
+// objectning name ni chiqaring.
+// 8. student object yarating. Ichida: name va subjects (array) bo‘lsin. subjects ichida 2 ta fan yozing.
+// Keyin birinchi fanni chiqaring.
+// 9. company object yarating. Ichida: name va employees (array) bo‘lsin. employees ichida 2 ta
+// object (name va role) bo‘lsin. Keyin ikkinchi employee role ni chiqaring.
+// 10. user object yarating. Ichida: name, hobbies (array), address (object) bo‘lsin. address ichida city
+// yozing. Keyin hobbies[0] va city ni chiqaring.
