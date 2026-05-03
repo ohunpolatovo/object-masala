9d19e6090d7414964432255e6a9851f9d279eb31
olloberdi <ohunpolatov.o@gmail.com>
2026-05-03 10:19:21 +0500

    object masalalar

--- main.js
+++ main.js
@@ -1,41 +1,95 @@
 // 1. user nomli object yarating. Ichida: name va age bo‘lsin. Keyin faqat name ni console.log qiling.
-let user = {
-  name: "Olloberdi",
-  age: 19,
-};
-console.log(user.name);
+// let user = {
+//   name: "Olloberdi",
+//   age: 19,
+// };
+// console.log(user.name);
 
 // 2. product object yarating. Ichida: title va price bo‘lsin. Keyin price ni chiqaring.
-let product = {
-  title: "Najot talim",
-  price: 1200,
-};
-console.log(product.price);
+// let product = {
+//   title: "Najot talim",
+//   price: 1200,
+// };
+// console.log(product.price);
 
 // 3. student object yarating. Ichida: name va active (true yoki false) bo‘lsin. Keyin active ni chiqaring.
-let student = {
-  name: "Olloberdi",
-  active: true,
-};
-console.log(student.active);
+// let student = {
+//   name: "Olloberdi",
+//   active: true,
+// };
+// console.log(student.active);
 
 // 4. car object yarating. Ichida: brand, color, year bo‘lsin. Keyin color ni chiqaring.
-let car = {
-  brand: "Malibu",
-  color: "black",
-  year: 2025,
-};
-console.log();
+// let car = {
+//   brand: "Malibu",
+//   color: "black",
+//   year: 2025,
+// };
+// console.log(car.color);
 
 // 5. phone object yarating. Ichida: brand va model bo‘lsin. Keyin model ni ['model'] orqali chiqaring.
-let phone = {};
+// let phone = {
+//   brand: "BmW",
+//   model: "BmW M5 Competion",
+// };
+// console.log(phone["model"]);
+
 // 6. user object yarating. Ichida: name va address object bo‘lsin. address ichida: city bo‘lsin. Keyin
 // city ni chiqaring.
+// let user = {
+//   name: "Olloberdi",
+//   addres: {
+//     city: "Andijon",
+//   },
+// };
+// console.log(user.addres.city);
+
 // 7. users nomli array yarating. Ichida 2 ta object bo‘lsin (name va age bilan). Keyin birinchi
 // objectning name ni chiqaring.
+// let users = [
+//   {
+//     name: "Olloberdi",
+//     age: 19,
+//   },
+//   {
+//     name: "Husanboy",
+//     age: 17,
+//   },
+// ];
+// console.log(users[0].name);
+
 // 8. student object yarating. Ichida: name va subjects (array) bo‘lsin. subjects ichida 2 ta fan yozing.
 // Keyin birinchi fanni chiqaring.
+// let student = {
+//   name: "Olloberdi",
+//   subjects: ["IT", "English"],
+// };
+// console.log(student.subjects[0]);
+
 // 9. company object yarating. Ichida: name va employees (array) bo‘lsin. employees ichida 2 ta
 // object (name va role) bo‘lsin. Keyin ikkinchi employee role ni chiqaring.
-// 10. user object yarating. Ichida: name, hobbies (array), address (object) bo‘lsin. address ichida city
-// yozing. Keyin hobbies[0] va city ni chiqaring.
+// let company = {
+//   name: "Epam",
+//   employess: [
+//     {
+//       name: "Olloberdi",
+//       role: "Frontend developer",
+//     },
+//     {
+//       name: "Husanboy",
+//       role: "Backend developer",
+//     },
+//   ],
+// };
+// console.log(company.employess[1].role);
+
+// 10. user object yarating. Ichida: name, hobbies (array), address (object) bo‘lsin.
+//  address ichida city yozing. Keyin hobbies[0] va city ni chiqaring.
+// let user = {
+//   name: "Olloberdi",
+//   hobbies: ["Futbol"],
+//   addres: {
+//     city: "Andijon",
+//   },
+// };
+// console.log(user.hobbies[0], user.addres.city);
